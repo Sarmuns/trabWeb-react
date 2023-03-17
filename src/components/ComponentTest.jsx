1451e8a351151d12b1fbb38653a6911dfdc1fcfb
Samuel Alcântara Fontenele Rocha <samuel.rocha@dellead.com>
2023-03-17 00:19:08 -0300

    first commit

---
+++ src/components/ComponentTest.jsx
@@ -0,0 +1,5 @@
+function ComponentTest(){
+    return(<h1>Essa eh a pagina FAQ</h1>)
+}
+
+export default ComponentTest
---
+++ src/pages/Faq.jsx
@@ -0,0 +1,9 @@
+//Você vai importar os componentes e montar eles na página FAQ aqui
+
+import ComponentTest from "../components/ComponentTest"
+
+function Faq(){
+    return(<ComponentTest />)
+}
+
+export default Faq
