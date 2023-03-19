f062d542fd236480c4ef445bca25059bb54e791b
_julianogueira3 <julia.mjn@icloud.com>
2023-03-19 15:12:00 -0300

    faq

--- src/components/Ask.jsx
+++ src/components/Ask.jsx
@@ -1,106 +1,126 @@
 import './Ask.css';
-import React, { useState } from 'react';
-function Ask(){
-    const [checked1, setChecked1] = useState(false);
-    const [checked2, setChecked2] = useState(false);
-    const [checked3, setChecked3] = useState(false);
-    const [checked4, setChecked4] = useState(false);
-    const [checked5, setChecked5] = useState(false);
-    const [checked6, setChecked6] = useState(false);
-    const [checked7, setChecked7] = useState(false);
+import $ from 'jquery';
+import React, { useEffect } from 'react';
 
-    const handleToggle1 = () => {
-    setChecked1(!checked1);
-    };
-    const handleToggle2 = () => {
-    setChecked2(!checked2);
-    };
-    const handleToggle3 = () => {
-    setChecked3(!checked3);
-    };
-    const handleToggle4 = () => {
-    setChecked4(!checked4);
-    };
-    const handleToggle5 = () => {
-    setChecked5(!checked5);
-    };
-    const handleToggle6 = () => {
-    setChecked6(!checked6);
-    };
-    const handleToggle7 = () => {
-    setChecked7(!checked7);
-    };
-    return(
-        <div className="faq">
-            <h2 className="text-center">Ajuda rápida:</h2>
-            <ul className="faq-menu-items text-center">
-                <li className="faq-menu-item">
-                    <input type="checkbox" id="faq-1" className="faq-toggle" checked={checked1} onChange={handleToggle1}/>
-                        <label htmlFor="faq-1" className="faq-item-link" >
-                            Eu me inscrevi. Por que não estou obtendo recursos Premium?
-                        </label>
-                        <div className="faq-answer">
-                            <p>A sua conta pode não estar configurada corretamente. Tente verificar suas configurações e entrar em contato com o suporte ao cliente se continuar tendo problemas.</p>
-                        </div>
-                </li>
-                <li className="faq-menu-item">
-                    <input type="checkbox" id="faq-2" className="faq-toggle" checked={checked2} onChange={handleToggle2}/>
-                        <label htmlFor="faq-2" className="faq-item-link" >
-                            Como posso pagar pelo Spotify?
-                        </label>
-                        <div className="faq-answer">
-                            <p>O Spotify aceita diversos métodos de pagamento, incluindo cartão de crédito e débito, PayPal, boleto bancário e outros meios disponíveis em sua região. Para verificar as opções de pagamento disponíveis para você, acesse as configurações de pagamento na sua conta do Spotify.</p>
-                        </div>
-                </li>
-        
-                <li className="faq-menu-item">
-                    <input type="checkbox" id="faq-3" className="faq-toggle" checked={checked3} onChange={handleToggle3}/>
-                        <label htmlFor="faq-3" className="faq-item-link" >
-                            Como altero meu endereço de e-mail?
-                        </label>   
-                        <div className="faq-answer">
-                            <p>Para alterar o endereço de e-mail associado à sua conta do Spotify, acesse as configurações da conta e selecione a opção "Editar perfil". Em seguida, clique em "Editar" ao lado do campo de e-mail e insira o novo endereço de e-mail. Certifique-se de confirmar as alterações clicando em "Salvar perfil" antes de sair.</p>
-                        </div>
-                </li>
-                <li lass="faq-menu-item">
-                        <input type="checkbox" id="faq-4" className="faq-toggle" checked={checked4} onChange={handleToggle4}/>
-                            <label htmlFor="faq-4" className="faq-item-link" >
-                                Criei uma nova conta. Posso transferir minhas playlists, músicas salvas e seguidores?
-                            </label>
-                            <div className="faq-answer">
-                                <p>Sim, é possível transferir suas playlists, músicas salvas e seguidores de uma conta antiga para uma nova. Para fazer isso, você precisará entrar em contato com o suporte ao cliente do Spotify e solicitar a transferência de dados. Este processo pode levar alguns dias para ser concluído.</p>
-                            </div>
-                </li>
-                <li className="faq-menu-item">
-                    <input type="checkbox" id="faq-5" className="faq-toggle" checked={checked5} onChange={handleToggle5}/>
-                        <label htmlFor="faq-5" className="faq-item-link" >
-                            Como funciona a oferta Premium de 1 mês grátis?
-                        </label>
-                        <div className="faq-answer">
-                            <p>A oferta Premium de 1 mês grátis é uma promoção oferecida pelo Spotify para novos usuários que desejam experimentar o serviço Premium. Para aproveitar a oferta, basta criar uma nova conta no Spotify e selecionar a opção Premium durante o período de inscrição. Você poderá desfrutar de todos os recursos Premium do Spotify gratuitamente por um mês inteiro.</p>
-                        </div>
-                </li>
-                <li className="faq-menu-item">
-                    <input type="checkbox" id="faq-6" className="faq-toggle" checked={checked6} onChange={handleToggle6}/>
-                        <label htmlFor="faq-6" className="faq-item-link" >
-                            Como solucionar problemas do Spotify no PlayStation?
-                        </label>
-                        <div className="faq-answer">
-                            <p>Se você está tendo problemas para usar o Spotify no PlayStation, verifique se o aplicativo está atualizado para a versão mais recente. Certifique-se também de que sua conta do PlayStation Network esteja conectada à sua conta do Spotify. Se ainda estiver enfrentando problemas, tente reiniciar o PlayStation e o roteador de internet para resolver problemas de conexão.</p>
-                        </div>
-                </li>
-                <li className="faq-menu-item">
-                    <input type="checkbox" id="faq-7" className="faq-toggle" checked={checked7} onChange={handleToggle7}/>
-                        <label htmlFor="faq-7" className="faq-item-link" >
-                            O que é sessão privada e como isso afeta minhas recomendações? 
-                        </label>
-                        <div className="faq-answer">
-                            <p>A sessão privada é uma opção disponível no Spotify que permite aos usuários ouvirem música sem que suas escolhas afetem suas recomendações de músicas. Quando a sessão privada está ativada, o Spotify não registra as músicas ouvidas pelo usuário, não atualiza as playlists do usuário com base em suas escolhas e não usa as inhtmlFormações de histórico de escuta do usuário para personalizar suas recomendações.</p>
-                        </div>
-                    
-                </li>
-            </ul>
-        </div>
+
+function Ask(){
+   
+    useEffect(() => {
+        $(document).ready(function() {
+          $('.accordion-button').click(function() {
+            const target = $(this).attr('data-bs-target');
+            if ($(target).hasClass('show')) {
+              $(this).removeClass('collapsed');
+              $(target).slideUp('fast');
+              $(target).removeClass('show');
+            } else {
+              $('.accordion-button').removeClass('collapsed');
+              $('.accordion-collapse').slideUp('fast');
+              $('.accordion-collapse').removeClass('show');
+              $(this).addClass('collapsed');
+              $(target).slideDown('fast', function() {
+                $(this).addClass('show');
+              });
+            }
+          });
+        });
+      }, []);
+      
+      
+      
+    
+      
+    
+return(
+    <div className="faq bg-dark">
+        <h2 className="text-center">Ajuda rápida:</h2>
+        <div className="accordion" id="accordionExample">
+          <div className="accordion-item">
+            <h2 className="accordion-header" id="headingOne">
+              <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                Eu me inscrevi. Por que não estou obtendo recursos Premium?
+              </button>
+            </h2>
+            <div id="collapseOne" className="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+              <div className="accordion-body">
+                A sua conta pode não estar configurada corretamente. Tente verificar suas configurações e entrar em contato com o suporte ao cliente se continuar tendo problemas.
+              </div>
+            </div>
+          </div>
+          <div className="accordion-item">
+            <h2 className="accordion-header" id="headingTwo">
+              <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                Como posso pagar pelo Spotify?
+              </button>
+            </h2>
+            <div id="collapseTwo" className="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+              <div className="accordion-body">
+                O Spotify aceita diversos métodos de pagamento, incluindo cartão de crédito e débito, PayPal, boleto bancário e outros meios disponíveis em sua região. Para verificar as opções de pagamento disponíveis para você, acesse as configurações de pagamento na sua conta do Spotify.
+              </div>
+            </div>
+          </div>
+          <div className="accordion-item">
+            <h2 className="accordion-header" id="headingThree">
+              <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                Como altero meu endereço de e-mail?
+              </button>
+            </h2>
+            <div id="collapseThree" className="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+              <div className="accordion-body">
+                Para alterar o endereço de e-mail associado à sua conta do Spotify, acesse as configurações da conta e selecione a opção "Editar perfil". Em seguida, clique em "Editar" ao lado do campo de e-mail e insira o novo endereço de e-mail. Certifique-se de confirmar as alterações clicando em "Salvar perfil" antes de sair.
+              </div>
+            </div>
+          </div>
+            <div className="accordion-item">
+                <h2 className="accordion-header" id="headingFour">
+                    <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+                        Criei uma nova conta. Posso transferir minhas playlists, músicas salvas e seguidores?
+                    </button>
+                </h2>
+            </div>
+            <div id="collapseFour" className="accordion-collapse collapse" aria-labelledby="headingFour"  data-bs-parent="#accordionExample">
+            <div className="accordion-body">
+                 Sim, é possível transferir suas playlists, músicas salvas e seguidores de uma conta antiga para uma nova. Para fazer isso, você precisará entrar em contato com o suporte ao cliente do Spotify e solicitar a transferência de dados. Este processo pode levar alguns dias para ser concluído.
+              </div>
+            </div>
+            <div className="accordion-item">
+                <h2 className="accordion-header" id="headingFive">
+                    <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
+                    Como funciona a oferta Premium de 1 mês grátis?
+                    </button>
+                </h2>
+            </div>
+            <div id="collapseFive" className="accordion-collapse collapse" aria-labelledby="headingFive"  data-bs-parent="#accordionExample">
+                <div className="accordion-body">
+                A oferta Premium de 1 mês grátis é uma promoção oferecida pelo Spotify para novos usuários que desejam experimentar o serviço Premium. Para aproveitar a oferta, basta criar uma nova conta no Spotify e selecionar a opção Premium durante o período de inscrição. Você poderá desfrutar de todos os recursos Premium do Spotify gratuitamente por um mês inteiro.
+                </div>
+            </div>
+            <div className="accordion-item">
+                <h2 className="accordion-header" id="headingSix">
+                    <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseSix" aria-expanded="false" aria-controls="collapseSix">
+                    Como solucionar problemas do Spotify no PlayStation?
+                    </button>
+                </h2>
+            </div>
+            <div id="collapseSix" className="accordion-collapse collapse" aria-labelledby="headingSix"  data-bs-parent="#accordionExample">
+                <div className="accordion-body">
+                Se você está tendo problemas para usar o Spotify no PlayStation, verifique se o aplicativo está atualizado para a versão mais recente. Certifique-se também de que sua conta do PlayStation Network esteja conectada à sua conta do Spotify. Se ainda estiver enfrentando problemas, tente reiniciar o PlayStation e o roteador de internet para resolver problemas de conexão.
+                </div>
+            </div>
+            <div className="accordion-item">
+                <h2 className="accordion-header" id="headingSeven">
+                    <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseSeven" aria-expanded="false" aria-controls="collapseSeven">
+                    O que é sessão privada e como isso afeta minhas recomendações? 
+                    </button>
+                </h2>
+            </div>
+            <div id="collapseSeven" className="accordion-collapse collapse" aria-labelledby="headingSeven"  data-bs-parent="#accordionExample">
+                <div className="accordion-body">
+                A sessão privada é uma opção disponível no Spotify que permite aos usuários ouvirem música sem que suas escolhas afetem suas recomendações de músicas. Quando a sessão privada está ativada, o Spotify não registra as músicas ouvidas pelo usuário, não atualiza as playlists do usuário com base em suas escolhas e não usa as inhtmlFormações de histórico de escuta do usuário para personalizar suas recomendações.
+                </div>
+            </div>
+        </div>      
+    </div>   
     );
 }
 
