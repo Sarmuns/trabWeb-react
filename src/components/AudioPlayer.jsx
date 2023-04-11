f1065499ae05e0c40cbc54e673d6086bfdc7256d
Samuel Rocha <samuel.rocha@dellead.com>
2023-04-11 19:55:16 -0300

    help

---
+++ src/components/AudioPlayer.jsx
@@ -0,0 +1,39 @@
+import React, { useState, useRef } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlay, faPause } from '@fortawesome/free-solid-svg-icons'
+
+
+const AudioPlayer = ({ audioPath }) => {
+    const [isPlaying, setIsPlaying] = useState(false);
+    const audioRef = useRef(new Audio(audioPath));
+
+    const handlePlay = () => {
+        audioRef.current.play();
+        setIsPlaying(true);
+    };
+
+    const handlePause = () => {
+        audioRef.current.pause();
+        setIsPlaying(false);
+    };
+
+    const stopAudio = () => {
+        audioRef.current.pause();
+        audioRef.current.currentTime = 0;
+        setIsPlaying(false);
+    }
+
+    return (
+        <div>
+            {isPlaying ? (
+                <>
+                    <FontAwesomeIcon onClick={handlePause} icon={faPause} />
+                </>
+            ) : (
+                <FontAwesomeIcon onClick={() => {stopAudio(); handlePlay()}} icon={faPlay} />
+            )}
+        </div>
+    );
+};
+
+export default AudioPlayer;
--- src/components/Music.jsx
+++ src/components/Music.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import AudioPlayer from "./AudioPlayer";
 import { useParams } from 'react-router-dom';
 
 const Music = (props) => {
@@ -17,7 +18,7 @@ const Music = (props) => {
             <tbody>
                 {data.playlists[id].musicas.map((musica, i) => (
                     <tr>
-                        <th scope="row">{i + 1}</th>
+                        <th scope="row"><AudioPlayer audioPath={musica.audioFile}/></th>
                         <td>{musica.nome}</td>
                         <td>{musica.artista}</td>
                         <td>{musica.album}</td>
