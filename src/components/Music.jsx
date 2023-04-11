1ee6551dfa5ba090e1068eda62b47566b10ea21f
Samuel Alcântara Fontenele Rocha <samuel.rocha@dellead.com>
2023-04-11 14:20:40 -0300

    Creating Music component

---
+++ src/components/Music.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { useParams } from 'react-router-dom';
+
+const Music = (props) => {
+    const { data } = props;
+    const { id } = useParams();
+    return (<>
+        <table class="table table-dark mb-0">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Nome</th>
+                    <th scope="col">Artista</th>
+                    <th scope="col">Album</th>
+                </tr>
+            </thead>
+            <tbody>
+                {data.playlists[id].musicas.map((musica, i) => (
+                    <tr>
+                        <th scope="row">{i+1}</th>
+                        <td>{musica.nome}</td>
+                        <td>{musica.artista}</td>
+                        <td>{musica.album}</td>
+                    </tr>
+
+                ))}
+            </tbody>
+        </table>
+
+    </>
+    )
+}
+
+export default Music
