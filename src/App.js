1082f49793c3ebdb42f8e57f5e67162a03331e24
Samuel Alcântara Fontenele Rocha <samuel.rocha@dellead.com>
2023-04-12 00:22:41 -0300

    Finally conditioning rendering of header and ftr

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import Header from './components/Header';
 import Footer from './components/Footer/Footer';
 import NotFound from './pages/NotFound';
@@ -12,33 +12,46 @@ import {
   Route,
   BrowserRouter
 } from "react-router-dom";
-import { useEffect } from "react";
+
+const Layout = ({ children }) => (
+  <>
+    <Header />
+    {children}
+    <Footer />
+  </>
+);
+
 
 function App() {
-  const [showHeader, setShowHeader] = useState(false);
-  useEffect(()=>{
-    setShowHeader(true);
-  },[]);
-  // useEffect(() => {
-  //   let showHeader = ['/', '/cadastro', '/faq', '/playlist'].includes(window.location.pathname);
-  //   console.log(`pathname: ${window.location.pathname}`)
-  //   console.log(showHeader);
-  //   setShowHeader(showHeader);
-  // }, [window.location]);
-  
 
   return (
     <BrowserRouter>
-      {showHeader ? <Header /> : ''}
       <Routes>
-        <Route path="/" element={<Home className="home" />} />
-        <Route path="/cadastro" element={<Cadastro className="cadastro" />} />
-        <Route path="/faq" element={<Faq className="faq" />} />
-        <Route path="/playlist" element={<Playlist className="playlist" />} />
-        <Route path="/playlist/:id" element={<PlaylistMusics />} />
-        <Route path="*" element={<NotFound />} />
+        <Route
+          path="/"
+          element={<Layout><Home className="home" /></Layout>}
+        />
+        <Route
+          path="/cadastro"
+          element={<Layout><Cadastro className="cadastro" /></Layout>}
+        />
+        <Route
+          path="/faq"
+          element={<Layout><Faq className="faq" /></Layout>}
+        />
+        <Route
+          path="/playlist"
+          element={<Layout><Playlist className="playlist" /></Layout>}
+        />
+        <Route
+          path="/playlist/:id"
+          element={<PlaylistMusics />}
+        />
+        <Route
+          path="*"
+          element={<NotFound />}
+        />
       </Routes>
-      {showHeader ? <Footer /> : ''}
     </BrowserRouter>
 
   );
