3f77a63610aa1232840f374ea1873dc199669283
Samuel Alcântara Fontenele Rocha <samuel.rocha@dellead.com>
2023-04-12 00:04:38 -0300

    refac

--- src/components/AudioPlayer.jsx
+++ src/components/AudioPlayer.jsx
@@ -4,7 +4,7 @@ import { faPlay, faPause } from '@fortawesome/free-solid-svg-icons'
 
 const AudioPlayer = ({ audioPath, setAllPlayersToFalse, index, isPlaying }) => {
     const audioRef = useRef(new Audio(audioPath));
-    const [isP, setIsP] = useState(true)
+    const [isP, setIsP] = useState(true) //controlar a renderização dos icones
 
     const handlePlay = () => {
         setAllPlayersToFalse(index);
@@ -18,19 +18,12 @@ const AudioPlayer = ({ audioPath, setAllPlayersToFalse, index, isPlaying }) => {
     };
 
     useEffect(() => {
-        console.log('isPlaying  '+isPlaying)
-        console.log('index  '+index)
         if (isPlaying) {
             handlePlay();
         } else {
             handlePause();
         }
     }, [isPlaying]);
-
-    const stopAudio = () => {
-        audioRef.current.pause();
-        audioRef.current.currentTime = 0;
-    }
 
     return (
         <div>
@@ -39,7 +32,6 @@ const AudioPlayer = ({ audioPath, setAllPlayersToFalse, index, isPlaying }) => {
             ) : (
                 <FontAwesomeIcon onClick={handlePlay} icon={faPlay} />
             )}
-            <audio src={audioPath} />
         </div>
     );
 };
